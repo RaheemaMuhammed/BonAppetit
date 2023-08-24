2fd3e8b981b539b4c6ea5c2bc37e8cbf2b562c25
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2023-08-24 17:11:50 +0530

    bugs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -214,7 +214,7 @@ RAZOR_KEY_SECRET = os.getenv('RAZOR_KEY_SECRET')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/app/staticfiles/'
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -224,5 +224,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.CustomUser'
 MEDIA_URL = 'media/'
-MEDIA_ROOT='/app/media'
+MEDIA_ROOT='media/'
 CORS_ORIGIN_ALLOW_ALL = True
