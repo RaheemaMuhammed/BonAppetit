706bd40f63472c3e3b37ad8b5988c9ef9d59284c
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2023-08-24 22:12:11 +0530

    config bug

--- frontend/src/Axios/Instances/Instance.js
+++ frontend/src/Axios/Instances/Instance.js
@@ -7,7 +7,7 @@ export const axiosCommonInstance=axios.create({baseURL:'http://16.171.250.80/'})
 export const axiosUserInstance=axios.create({baseURL:'http://16.171.250.80/'})
 export const axiosAdminInstance = axios.create({baseURL:'http://16.171.250.80/adminpanel/'})
 
-export const axiosInstance= 'http://16.171.250.80/'
+export const axiosInstance= 'http://16.171.250.80'
 
-export const baseURLUser='http://16.171.250.80';
+export const baseURLUser='http://16.171.250.80/';
 export const baseURLAdmin='http://16.171.250.80/adminpanel/';
