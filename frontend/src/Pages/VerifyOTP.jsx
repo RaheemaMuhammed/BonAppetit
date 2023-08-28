1d2d38b65dc7ce0c290c06805a5cde5d670ff4fc
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2023-08-29 00:06:42 +0530

    renamed file

--- frontend/src/Pages/VerifyOTP.jsx
+++ frontend/src/Pages/VerifyOTP.jsx
@@ -3,6 +3,7 @@ import React,{useEffect,useState} from 'react'
 import Loader from '../Components/Loader'
 import OtpForm from '../Components/otpForm'
 
+
 const VerifyOTP = () => {
     const [loader, setLoader] = useState(true)
     useEffect(() => {
